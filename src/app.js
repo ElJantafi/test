b5eda6a1dc1654214c66195970367b0e3f986a25
secondJTFe <selfmanaging17@gmail.com>
2023-02-14 08:22:32 +0100

    first commit

--- src/app.js
+++ src/app.js
@@ -1,33 +1,46 @@
+let urls = [];
+fetch('https://api.netlify.com/api/v1/sites?access_token=F8WcEUqkXLFEFIPBnms3-5CzZhCH4IGycfUVuQPQF8g')
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(site => {
+      console.log(site.url);
+      urls.push(site.url);
+      console.log(urls)
+    })
+  })
+  .catch(error => console.error(error))
+
+
 const projects = [
     {
      id : 1 ,
      title : 'Leon agency' ,
      image : 'leon.png',
-     link : 'AllProjects/template1/index.html',
+     link : 'https://rococo-sundae-d83cdd.netlify.app/',
     },
     {
      id : 2 ,
      title : 'Jantafi World' ,
      image : 'jantafi.png',
-     link : '/AllProjects/template3/index.html'
+     link : 'https://storied-snickerdoodle-54cf0d.netlify.app/'
     },
     {
      id : 3 ,
      title : 'Restaurent Menu' ,
      image : 'menu.png' ,
-     link : 'AllProjects/Menu/index.html'
+     link : 'https://phenomenal-wisp-398218.netlify.app/'
     },
     {
      id : 4 ,
      title : 'Portfolio' ,
      image : 'dev103.png' ,
-     link : 'https://phenomenal-wisp-398218.netlify.app/'
+     link : 'https://dev103tech.netlify.app/'
     }, 
     {
       id : 5 ,
       title : 'Kasper Agency' ,
       image : 'kasper2.png' ,
-      link : 'AllProjects/template2/index.html'
+      link : 'https://iridescent-empanada-ce99f6.netlify.app/'
      },
     {
       id : 6 ,
@@ -45,7 +58,7 @@ const projects = [
       id : 8 ,
       title : 'Ecommerce Website' ,
       image : 'ryeh.png' ,
-      link : 'AllProjects/ryeh/index.html'
+      link : 'https://starlit-faloodeh-d6c548.netlify.app/'
      }
  ];
 
